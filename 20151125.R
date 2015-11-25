bddea303c77995256c4e24c9dc18104d8012bb65
PauEscarcia <paulina_151090@hotmail.com>
2015-11-25 12:43:40 -0600

    Diferents symbols in points

--- 20151125.R
+++ 20151125.R
@@ -4,13 +4,12 @@
 #and restart R
 
 # if it shows dependncy errors install the following libraries
-install.packages("bitops")
-install.packages("RCurl")
-install.packages("lubridate")
+#install.packages("bitops")
+#install.packages("RCurl")
+#install.packages("lubridate")
 
 # Use libraries
 library("lubridate")
-
 library("RCurl")
 
 # Read experiment data from google docs
@@ -30,8 +29,12 @@ boxplot(time~stage, data = allscores, main = "<TITLE>", xlab = "Stages", ylab = 
 #legend("topright", inset=.01, title="Stage Min",
 #       c("6","17","8"), fill=terrain.colors(3), horiz=TRUE)
 
-#points to automate
-points(1,22,col="red")
-points(2,19,col="gray")
-points(3,5,col="pink")
+#points to automate, points(x,y,color,pch=symbol)   
+
+points(1,22,col="red",pch=0:4)
+points(2,19,col="gray",pch=5:9)
+points(2,40,col="pink",pch=10:14)
+points(2,15,col="pink",pch=20:25)
+
+
 
