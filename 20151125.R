bdbccd3bee8a8d2fbce6b194ca4a0e3ba03b2245
agarciafdz <agarciafdz@gmail.com>
2015-11-25 09:53:03 -0600

    Import data directly from the Google Docs Spreadsheet

--- 20151125.R
+++ 20151125.R
@@ -1,54 +1,21 @@
-# Load into R the dataset. The structure of the file used in this sample is at the end of this script.
-setwd("/path/to/file/")
-allscores <- read.table("scores.txt", header = TRUE)
+# if it shows dependncy errors install the following libraries
+#install.packages("RCurl")
+#install.packages("lubridate")
+
+# Use libraries
+library("lubridate")
+library("RCurl")
+
+# Read experiment data from google docs
+spreadsheet.data <-getURL("https://docs.google.com/spreadsheets/d/1trZhg4iJmVPRrZR3IMv1xTUZx-8vMhXu1ihYJITWEKs/pub?gid=273369584&single=true&output=csv")
+
+#parse data as CSV
+allscores <- read.csv(textConnection(spreadsheet.data),
+	              colClasses=c("factor","factor","character"))
+		      
+#Change time column to integers with the number of seconds
+allscores$time=period_to_seconds(ms(allscores$time))
+
 # Plot a comparative box plot, indicating the variables to compare, the dataset and extra information like a title, x-y titles and colors.
 boxplot(time~stage, data = allscores, main = "<TITLE>", xlab = "Stages", ylab = "Participants", col = c("green", "white", "red"))
 
-# File structure used in this sample:
-# "scores.txt":
-# stage time
-# 1 6
-# 1 10
-# 1 8
-# 1 9
-# 1 8
-# 1 8
-# 1 14
-# 1 18
-# 1 10
-# 1 9
-# 1 14
-# 1 16
-# 1 11
-# 1 14
-# 1 18
-# 2 18
-# 2 24
-# 2 26
-# 2 30
-# 2 29
-# 2 31
-# 2 31
-# 2 49
-# 2 43
-# 2 17
-# 2 37
-# 2 34
-# 2 42
-# 2 37
-# 2 55
-# 3 8
-# 3 11
-# 3 10
-# 3 11
-# 3 9
-# 3 13
-# 3 11
-# 3 15
-# 3 8
-# 3 10
-# 3 14
-# 3 10
-# 3 10
-# 3 13
-# 3 13
